93fac01035fa4fb40f56422d6b19a92739fc48d9
Adam McKenney (AMM) <adam@quitetiny.com>
2014-06-10 08:34:12 -0400

    Added Help Class

---
+++ Classes/Help.vb
@@ -0,0 +1,18 @@
+﻿'This displays help and documentation
+Imports System.Console
+Class Help_Class
+    ReadOnly Name As String = "+Help"
+    Dim Func As String = Nothing
+    Public Sub Help()
+        Console.WriteLine("Foo")
+        Console.WriteLine("Bar")
+    End Sub
+
+    Public Sub Doc()
+        Shell("http://www.example.com/")
+    End Sub
+
+    Public Sub Man()
+        Console.WriteLine("Manual Page")
+    End Sub
+End Class
